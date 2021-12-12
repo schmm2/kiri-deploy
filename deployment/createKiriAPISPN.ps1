7728bdd4a548f6ec0b4f7a7c84d1659fe06eeb19
Martin Schmidli <m.schmidli@smartit.ch>
2021-12-12 22:01:16 +0100

    update

--- deployment/createKiriAPISPN.ps1
+++ deployment/createKiriAPISPN.ps1
@@ -1,12 +1,12 @@
 Invoke-Command {
-    $AppName = "svc-kiriapi666"
-    $ReplyUri = "https://testkiri5555-backend.azurewebsites.net/.auth/login/aad/callback"
+    $AppName = "svc-kiriapi777"
+    $ReplyUri = "https://testkiri777-backend.azurewebsites.net/.auth/login/aad/callback"
     $RequiredGrants = [Microsoft.Open.AzureAD.Model.RequiredResourceAccess]::new("00000003-0000-0000-c000-000000000000", @([Microsoft.Open.AzureAD.Model.ResourceAccess]::new("e1fe6dd8-ba31-4d61-89e7-88639da4683d", "Scope"), [Microsoft.Open.AzureAD.Model.ResourceAccess]::new("b340eb25-3456-403f-be2f-af7a0d370277", "Scope")))
     $AppHomePage = "https://kiri.codes"
 
     Connect-AzureAD
     
-    $App = New-AzureADApplication -DisplayName $AppName -AvailableToOtherTenants $true -RequiredResourceAccess $RequiredGrants -HomePage $AppHomePage -ReplyUrls $ReplyUri
+    $App = New-AzureADApplication -DisplayName $AppName -AvailableToOtherTenants $true -HomePage $AppHomePage -ReplyUrls $ReplyUri -RequiredResourceAccess $RequiredGrants
     Set-AzureADApplication -ObjectId $app.ObjectId -IdentifierUris ("api://" + $app.AppId)
     
     write-host "--------------------------------------------------------------------------"
